a1fea9bbd19ebb43268badb2e325310a8508eb2a
Erwin Ernst Steinhammer <eestoneee@gmail.com>
2017-04-09 02:18:17 +0200

    integretion of the national assembly daily updates #4

--- council-of-ministers-reports-front.js
+++ council-of-ministers-reports-front.js
@@ -3,7 +3,6 @@ var concil_of_ministers_url = doURL_YQL("https://www.bka.gv.at/ministerratsproto
 $.get( concil_of_ministers_url, function( data1 ) {
  var link1 = ($( "main.main .overview", data1 ).children().first()
   ).children().first().attr("href");
-
  $.get( doURL_YQL(link1), function( data2 ) {
   $( "#council-of-ministers-reports" ).html($( ".journal-content-article", data2 ).html());
  });
