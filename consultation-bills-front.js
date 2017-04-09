2623511986791a88b1e2408d8749d14aaf85d1be
Erwin Ernst Steinhammer <eestoneee@gmail.com>
2017-04-09 04:06:57 +0200

    save progress from #2

--- consultation-bills-front.js
+++ consultation-bills-front.js
@@ -1,2 +1,10 @@
-var test = "consultation bills";
-document.write(test);
+//$.get(RIS_appiUrl + 'Bundesgesetzblaetter', function ( data1 ) {
+//  var link1 = $( "channel guid", data1 ).first().html();
+//  $.get( doURL_YQL(link1), function( data2 ) {
+//    $($( "#consultation-bills" ).html( $( "body", data2 ).html() ));
+//  });
+//});
+
+$.get( RIS_appiUrl + "Bundesgesetzblaetter" + "?Application=Begut&ImRisSeit=EinerWoche", function ( data1 ) {
+  $($( "#consultation-bills" ).html( "davor " + JSON.stringify(data1) + " danach" ));
+});
--- national-assembly-daily-updates-front.js
+++ national-assembly-daily-updates-front.js
@@ -1,7 +1,6 @@
 var national_assembly_daily_updates_url = doURL_YQL("https://www.parlament.gv.at/PAKT/AKT/NRMAIL/filter.psp?view=RSS&jsMode=RSS&xdocumentUri=%2FPAKT%2FAKT%2FNRMAIL%2Findex.shtml&ITYP=NRMAIL&R_30JA=30&anwenden=Anwenden&listeId=114&FBEZ=FP_014");
 
 $.get(national_assembly_daily_updates_url, function ( data1 ) {
-//  $( "#national-assembly-daily-updates" ).html($( "#feedContent", data3 ).html());
   var link1 = $( "channel guid", data1 ).first().html();
   $.get( doURL_YQL(link1), function( data2 ) {
     $($( "#national-assembly-daily-updates" ).html( $( "body #content", data2 ).html() ));
