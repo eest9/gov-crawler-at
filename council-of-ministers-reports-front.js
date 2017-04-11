1efa71e0d4403849aaaca26b62126435f87b9bb5
Erwin Ernst Steinhammer <eestoneee@gmail.com>
2017-04-11 22:58:26 +0200

    prepere link-associations

--- council-of-ministers-reports-front.js
+++ council-of-ministers-reports-front.js
@@ -4,6 +4,8 @@ $.get( concil_of_ministers_url, function( data1 ) {
  var link1 = ($( "main.main .overview", data1 ).children().first()
   ).children().first().attr("href");
  $.get( doURL_YQL(link1), function( data2 ) {
-  $( "#council-of-ministers-reports" ).html($( ".journal-content-article", data2 ).html());
+   report_raw = $( ".journal-content-article", data2 ).html();
+   $( ".panel.council-of-ministers-reports h1.panel-title" ).html( $( "h1", report_raw ).html() );
+   $( "#council-of-ministers-reports" ).html( $( "p", report_raw).unwrap() ).append($( "ul", report_raw).unwrap());
  });
 });
