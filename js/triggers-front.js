07651dc13c599ee201e1c126e5f93189f14b6d7f
Erwin Ernst Steinhammer <eestoneee@gmail.com>
2017-04-18 21:08:09 +0200

    shortkeys

--- js/triggers-front.js
+++ js/triggers-front.js
@@ -1,3 +1,13 @@
 concil_of_ministers_report_front();
 consultation_bills_front();
 national_assembly_daily_updates_front();
+
+$("body").keyup(function( key ) {
+  if (key.which == 49) {
+    concil_of_ministers_report_front();
+  } else if (key.which == 50) {
+    consultation_bills_front();
+  } else if (key.which == 51) {
+    national_assembly_daily_updates_front();
+  }
+});
