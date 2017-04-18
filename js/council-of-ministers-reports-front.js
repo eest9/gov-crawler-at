05d9e9677a8e93e0a198c1480367179e699253d4
Erwin Ernst Steinhammer <eestoneee@gmail.com>
2017-04-18 19:16:22 +0200

    Impressum

--- js/council-of-ministers-reports-front.js
+++ js/council-of-ministers-reports-front.js
@@ -1,6 +1,6 @@
-var concil_of_ministers_url = doURL_YQL("https://www.bka.gv.at/ministerratsprotokolle");
+var council_of_ministers_url = doURL_YQL("https://www.bka.gv.at/ministerratsprotokolle");
 
-$.get( concil_of_ministers_url, function( data1 ) {
+$.get( council_of_ministers_url, function( data1 ) {
   var link1 = ($( "main.main .overview", data1 ).children().first()
   ).children().first().attr("href");
   $.get( doURL_YQL(link1), function( data2 ) {
